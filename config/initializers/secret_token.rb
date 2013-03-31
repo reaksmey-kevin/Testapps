bda52093a2049184c2d2a42273c8bb04eae791e9
reaksmey <reaksmey@reaksmey-Aspire-V5-471G.(none)>
2013-03-31 13:20:31 +0700

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApps::Application.config.secret_token = 'a90807ad5d1bca377aad8d1c2dbcaeeec3fe8ed2511ea62d07cb4b9071bde45dc1ec539142975e04cebe37b82f979e2292a5f5ad5bb4ce50973f0117573d6eda'
